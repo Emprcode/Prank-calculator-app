e07c453b646b0c3fd01b4d507afeb23ab8b52094
Narayan Aryal <aryal.coding@gmail.com>
2022-10-29 09:45:24 +1100

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { Display } from './components/Display';
-import { BtnArea } from './components/BtnArea';
-import { useState } from 'react';
-import abc from './components/abc.wav';
+import logo from "./logo.svg";
+import "./App.css";
+import { Display } from "./components/Display";
+import { BtnArea } from "./components/BtnArea";
+import { useState } from "react";
+import abc from "./components/abc.wav";
 const operators = ["%", "/", "*", "-", "+"];
 
 const App = () => {
@@ -13,96 +13,86 @@ const App = () => {
   const audio = new Audio(abc);
 
   const extraValue = () => {
-    const num = Math.round(Math.random() * 10)
+    const num = Math.round(Math.random() * 10);
     return num > 3 ? 0 : num;
-  }
+  };
 
-  const handleOnClick =(val) => {
-      console.log(val);
+  const handleOnClick = (val) => {
+    console.log(val);
 
-      isPrank && setIsPrank(false);
+    isPrank && setIsPrank(false);
 
-      if(val === "AC"){
-        setStr('')
-        return;
+    if (val === "AC") {
+      setStr("");
+      return;
     }
-    if (val === "C"){
-      const shortStr = str.slice(0,-1);
+    if (val === "C") {
+      const shortStr = str.slice(0, -1);
       return setStr(shortStr);
-
     }
 
-    if(val === "="){
-      const lastChar = str[str.length -1];
-      let strToDisplay = str
+    if (val === "=") {
+      const lastChar = str[str.length - 1];
+      let strToDisplay = str;
 
-      if (operators.includes(lastChar)){
-         strToDisplay = str.slice(0,-1);
+      if (operators.includes(lastChar)) {
+        strToDisplay = str.slice(0, -1);
       }
 
       const extra = extraValue();
 
-      extra &&  audio.play() && setIsPrank(true) ;
+      extra && audio.play() && setIsPrank(true);
       const ttl = eval(strToDisplay) + extra;
 
-     
-      setStr(ttl. toString());
+      setStr(ttl.toString());
       return;
-
-  }
-
-  
-  //prevent multiple operator
-
-  if(operators.includes(val)){
-    setlastOperator(val);
-
-    if(!str){
-        return;
-    };
-    const lastChar = str[str.length -1];
-
-    let tempStr = str
-    if (operators.includes(lastChar)){
-        tempStr = str.slice(0, -1);
     }
-    setStr(tempStr + val);
-    return;
-};
 
-// make sure no more then one dot
+    //prevent multiple operator
 
-if ( val === "."){
+    if (operators.includes(val)) {
+      setlastOperator(val);
 
-  if(lastOperator){
-      const operatorIndex = str.lastIndexOf(lastOperator);
-      const lastNumberSet = str.slice(operatorIndex + 1);
-      if (lastNumberSet.includes(".")){
-          return;
+      if (!str) {
+        return;
       }
-  }
+      const lastChar = str[str.length - 1];
 
-  if(!lastOperator && str.includes(".")){
+      let tempStr = str;
+      if (operators.includes(lastChar)) {
+        tempStr = str.slice(0, -1);
+      }
+      setStr(tempStr + val);
       return;
-  }
-}
+    }
 
+    // make sure no more then one dot
 
+    if (val === ".") {
+      if (lastOperator) {
+        const operatorIndex = str.lastIndexOf(lastOperator);
+        const lastNumberSet = str.slice(operatorIndex + 1);
+        if (lastNumberSet.includes(".")) {
+          return;
+        }
+      }
 
+      if (!lastOperator && str.includes(".")) {
+        return;
+      }
+    }
 
-
-      setStr(str + val);
-  }
+    setStr(str + val);
+  };
   return (
     <div className="wrapper">
-        <div className="calculator">
-            <Display string={str} isPrank= {isPrank}/>
-            <BtnArea handleOnClick={handleOnClick}/>
-           
-        </div>
-        <div className="ball"></div>
+      <div className="calculator">
+        <Display string={str} isPrank={isPrank} />
+        <BtnArea handleOnClick={handleOnClick} />
+      </div>
+      <div className="ball"></div>
     </div>
   );
-}
+};
 
 export default App;
